66b50e62ef688f9c40262f5f9affa582fc8c60df
damusnet <contact@damusnet.fr>
2012-08-16 17:30:24 +0200

    beautifying routes

--- Config/routes.php
+++ Config/routes.php
@@ -1,6 +1,15 @@
 <?php
 /* Paypal IPN plugin */
-Router::connect('/paypal_ipn/process', array('plugin' => 'paypal_ipn', 'controller' => 'instant_payment_notifications', 'action' => 'process'));
+Router::connect('/paypal_ipn/process', array(
+	'plugin' => 'paypal_ipn',
+	'controller' => 'instant_payment_notifications',
+	'action' => 'process'
+));
+
 /* Optional Route, but nice for administration */
-Router::connect('/paypal_ipn/:action/*', array('admin' => 'true', 'plugin' => 'paypal_ipn', 'controller' => 'instant_payment_notifications', 'action' => 'index'));
+Router::connect('/paypal_ipn/:action/*', array(
+	'admin' => 'true',
+	'plugin' => 'paypal_ipn',
+	'controller' => 'instant_payment_notifications',
+));
 /* End Paypal IPN plugin */
