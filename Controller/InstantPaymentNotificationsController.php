ab6076e77c1f68327ab64802653451299f725a52
Daniel Mendalka <daniel.mendalka@gmail.com>
2012-10-19 23:22:05 +0200

    if remote_ip() function don't exists, declare it

--- Controller/InstantPaymentNotificationsController.php
+++ Controller/InstantPaymentNotificationsController.php
@@ -136,5 +136,18 @@ class InstantPaymentNotificationsController extends PaypalIpnAppController {
 			$this->redirect(array('action'=>'index'));
 		}
 	}
+}
 
+if (!function_exists('remote_ip')) {
+	function remote_ip() {
+	  if (!empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+	  	return $_SERVER['HTTP_CF_CONNECTING_IP'];
+	  }
+
+	  if (!empty($_SERVER['REMOTE_ADDR'])) {
+	  	return $_SERVER['REMOTE_ADDR'];
+	  }
+
+	  return false;
+	}
 }
